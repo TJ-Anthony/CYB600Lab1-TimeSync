77a90a82e68d4f09ac58b69b30624ce53eb30643
mendrys1 <amendrysajr@gmail.com>
2024-02-10 15:45:08 -0500

    app.py finished (Not Working)

---
+++ src/csc_cyb600_TSL1/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_current_time():
+    current_time = datetime.datetime.now().strftime("%m/%d/%Y  %H:%M:%S")
+
+    return f"Current Time: {current_time}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
